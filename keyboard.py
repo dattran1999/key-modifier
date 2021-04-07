30ca9a6796f4ac4eb62fcee6d75f03b39c1fb188
dattran1999 <z5210945@student.unsw.edu.au>
2021-04-08 00:52:05 +1000

    Initial commit. Added files to track keyboard and mouse events + basic server

---
+++ keyboard.py
@@ -0,0 +1,16 @@
+from pynput.keyboard import Key, Listener
+
+def on_press(key):
+    print(f'{key} pressed')
+
+def on_release(key):
+    print(f'{key} release')
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
