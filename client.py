90faaf05f13341d70dcbcffa66fab03894f4c4ab
dattran1999 <z5210945@student.unsw.edu.au>
2021-04-08 15:54:07 +1000

    Able to listen to key press on machine A and send that to machine B. Machine B will press that button

--- client.py
+++ client.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python3
 
-import sys
 from socket import socket, AF_INET, SOCK_DGRAM, gethostbyname_ex, gethostname
+from pynput.keyboard import Key, Listener
 
 # Find the local IP address
 # https://stackoverflow.com/a/1267524
 SERVER_IP   = [l for l in ([ip for ip in gethostbyname_ex(gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket(AF_INET, SOCK_DGRAM)]][0][1]]) if l][0][0]
 PORT_NUMBER = 5000
+
+SERVER = (SERVER_IP, PORT_NUMBER)
 SIZE = 1024
-print ("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
+print ("Client sending packets to {0}:{1}\n".format(SERVER_IP, PORT_NUMBER))
 
-mySocket = socket( AF_INET, SOCK_DGRAM )
-myMessage = "Hello!"
-myMessage1 = ""
-i = 0
-while i < 10:
-    mySocket.sendto(myMessage.encode('utf-8'),(SERVER_IP,PORT_NUMBER))
-    i = i + 1
+msg_socket = socket( AF_INET, SOCK_DGRAM )
 
-mySocket.sendto(myMessage1.encode('utf-8'),(SERVER_IP,PORT_NUMBER))
+def on_press(key):
+    msg = None
+    try:
+        msg = '{0} pressed'.format(key.char)
+    except:
+        msg = '{0} pressed'.format(key)
+    print(msg)
+    msg_socket.sendto(msg.encode('utf-8'), SERVER)
 
-sys.exit()
+def on_release(key):
+    msg = None
+    try:
+        msg = '{0} released'.format(key.char)
+    except:
+        msg = '{0} released'.format(key)
+    print(msg)
+    msg_socket.sendto(msg.encode('utf-8'), SERVER)
+
+# Collect events until released
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
--- server.py
+++ server.py
@@ -1,16 +1,44 @@
 from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
+from pynput.keyboard import Controller, Key
+import json
 import sys
+
+# Set up server listen for input instructions
 PORT_NUMBER = 5000
 SIZE = 1024
 
 hostName = gethostbyname( '0.0.0.0' )
 
-mySocket = socket( AF_INET, SOCK_DGRAM )
-mySocket.bind( (hostName, PORT_NUMBER) )
+msg_socket = socket( AF_INET, SOCK_DGRAM )
+msg_socket.bind( (hostName, PORT_NUMBER) )
 
+# keyboard controller
+keyboard = Controller()
 print("Test server listening on port {0}\n".format(PORT_NUMBER))
 
+def get_key_enum(key):
+    key_name = key.split('.')[1]
+    return getattr(Key, key_name)
+
 while True:
-    (data,addr) = mySocket.recvfrom(SIZE)
-    print(data)
+    (data,addr) = msg_socket.recvfrom(SIZE)
+    data = data.decode('utf-8')
+    # data is in form: {key} {type}
+    # e.g. 'a' pressed
+    res = data.split(' ')
+    print(res)
+    if res[1] == 'released':
+        try:
+            keyboard.release(res[0])
+        except ValueError:
+            key_obj = get_key_enum(res[0])
+            keyboard.release(key_obj)
+
+    elif res[1] == 'pressed':
+        try:
+            keyboard.press(res[0])
+        except ValueError:
+            key_obj = get_key_enum(res[0])
+            keyboard.press(key_obj)
+
 sys.exit()
