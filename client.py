9ff27f68a7f92bf6f9ad41abf368376286189141
dattran1999 <z5210945@student.unsw.edu.au>
2021-04-08 14:50:25 +1000

    Dynamic way of figuring out IP address of machine

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import sys
-from socket import socket, AF_INET, SOCK_DGRAM
+from socket import socket, AF_INET, SOCK_DGRAM, gethostbyname_ex, gethostname
 
-SERVER_IP   = '192.168.1.104'
+# Find the local IP address
+# https://stackoverflow.com/a/1267524
+SERVER_IP   = [l for l in ([ip for ip in gethostbyname_ex(gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket(AF_INET, SOCK_DGRAM)]][0][1]]) if l][0][0]
 PORT_NUMBER = 5000
 SIZE = 1024
 print ("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
