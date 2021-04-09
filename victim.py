220a05fb81a1bb774d93e4ebea3e05aa04a8c704
dattran1999 <z5210945@student.unsw.edu.au>
2021-04-10 00:18:56 +1000

    If can't find IP address of machine, use ifconfig and read from there...

--- victim.py
+++ victim.py
@@ -1,4 +1,5 @@
-from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
+import os
+from socket import socket, gethostbyname, gethostname, AF_INET, SOCK_DGRAM
 from pynput.keyboard import Controller, Key
 import json
 import sys
@@ -9,11 +10,16 @@ SIZE = 1024
 try:
     # Find IP address of this machine
     # https://stackoverflow.com/a/1267524
-    SERVER_IP   = [l for l in ([ip for ip in gethostbyname_ex(gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket(AF_INET, SOCK_DGRAM)]][0][1]]) if l][0][0]
+    SERVER_IP = [l for l in ([ip for ip in gethostbyname_ex(gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket(AF_INET, SOCK_DGRAM)]][0][1]]) if l][0][0]
 
 except:
-    # hardcode...
-    SERVER_IP   = '192.168.1.104'
+    try:
+        # Otherwise use a simpler approach
+        SERVER_IP = gethostbyname(gethostname())
+    except:
+        # call ifconfig and read from there
+        os.system('ifconfig')
+        SERVER_IP   = 'undefined'
 
 hostName = gethostbyname( '0.0.0.0' )
 
